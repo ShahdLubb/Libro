7b7bded05bbe591290cbfa5142d427dc814eb292
Shahd <shahdlubb@gmail.com>
2023-07-19 13:34:26 +0300

    remove azure keyVault service

--- Web/Program.cs
+++ Web/Program.cs
@@ -4,8 +4,6 @@ using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
-builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
 
 // Add services to the container.
 
